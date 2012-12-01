59450891b823b33f23613eef154b9539dd9d4950
Luis R. Rodriguez <mcgrof@do-not-panic.com>
2012-11-30 17:58:31 -0800

    alx: extend help menu
    Provide clear instructions as to what is required.
    To make things easier for users make the linux target
    depend on the linux-src target.
    
    Signed-off-by: Luis R. Rodriguez <mcgrof@do-not-panic.com>

--- target/linux/port.mk
+++ target/linux/port.mk
@@ -3,17 +3,28 @@ TARGET_LINUX=target/linux/
 all: help
 
 linux-help:
+	@echo "======================================================================="
+	@echo "Linux build instructions"
+	@echo ""
+	@echo "If you want to build and install alx you should issue these commands:"
+	@echo ""
+	@echo "    make linux"
+	@echo "    make install-linux"
+	@echo ""
+	@echo "The full list of Linux targets availale:"
+	@echo ""
 	@echo "    make linux-src              - Transforms code for integration into linux-next"
 	@echo "    make linux                  - Builds alx for any Linux kernel 2.6.28 - 3.x"
 	@echo "    make install-linux          - Install linux target"
 	@echo "    make uninstall-linux        - Install linux target"
 	@echo "    make clean-linux            - Install linux target"
+	@echo "======================================================================="
 
 # Convert unified driver code to Linux, always targeting linux-next
 linux-src:
 	@cp -a src $(TARGET_LINUX)
 
-linux-backport:
+linux-backport: linux-src
 	@-cd $(TARGET_LINUX) ; \
 	  ./refresh-compat ; \
 	  patch -N -p6 -d src/ < patches/unified-drivers/network/0001-backport-alx.patch
